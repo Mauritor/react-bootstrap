9044bf8e82874dedbf95b533e80145a2c637242c
Mauro <mrtelefonia@gmail.com>
2020-11-04 12:04:32 -0300

    Agregado de componente ImageMain

---
+++ src/components/ImageMain.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from 'react';
+
+const ImageMain = () => {
+    return (
+        <Fragment>
+            <div className="content-main">
+                <h1 className="content-main-text">Hello world!</h1>
+            </div>
+        </Fragment>
+    )
+}
+export default ImageMain
