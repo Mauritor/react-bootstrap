5060a90a09dbc1d49751596d8574486726675a9b
Mauro <mrtelefonia@gmail.com>
2020-11-04 16:33:49 -0300

    Agregado de dependencia Typed.js y usada en en componenteImageMain

--- src/components/ImageMain.js
+++ src/components/ImageMain.js
@@ -1,10 +1,26 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useEffect } from 'react';
+import Typed from 'typed.js';
 
 const ImageMain = () => {
+    useEffect(() => {
+        const options = {
+            strings: ['World!', 'I said Hello again'],
+            typeSpeed: 50,
+            backSpeed: 80,
+            backDelay: 3000,
+            startDelay: 1000,
+            loop: true,
+            showCursor: true
+        }
+        const typed = new Typed('#demo', options)
+        return () => {
+            typed.destroy();
+        }
+    }, [])
     return (
         <Fragment>
             <div className="content-main">
-                <h1 className="content-main-text">Hello world!</h1>
+                <h1 className="content-main-text">Hello <span id="demo"></span></h1>
             </div>
         </Fragment>
     )
